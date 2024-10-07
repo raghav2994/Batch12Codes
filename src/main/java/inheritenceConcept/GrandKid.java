aa8a1af634161a41d482bba050ea829850c1b6db
Arijit-G <arijitg508@gmail.com>
2024-10-08 00:55:06 +0530

    Commited by Arijit

--- src/main/java/inheritenceConcept/GrandKid.java
+++ src/main/java/inheritenceConcept/GrandKid.java
@@ -1,13 +1,16 @@
 package inheritenceConcept;
 
 public class GrandKid extends Kid{
-
+        
     public static void main(String[] args){
         GrandKid gk = new GrandKid();
         gk.landOfFather();
         gk.landOfGrandFather();
-        gk.grandKidEat();
+        //gk.grandKidEat();
     } 
+    {
+    System.out.println("Hello!Commited By arijit");
+    }
  
     public void grandKidEat(String pizza){
 		System.out.println("My favourite food is   " + pizza );
